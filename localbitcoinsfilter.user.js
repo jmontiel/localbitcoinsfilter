bb8ea76a41d37cac82b20475dab7f8a3a89e3d0d
José Montiel <jmontiel@users.noreply.github.com>
2018-01-27 14:04:47 -0400

    Update localbitcoinsfilter.user.js

--- localbitcoinsfilter.user.js
+++ localbitcoinsfilter.user.js
@@ -43,18 +43,18 @@
  *   of the <td> itself.
  */
 jQuery.fn.sortElements = (function(){
-    
+
     var sort = [].sort;
-    
+
     return function(comparator, getSortable) {
-        
+
         getSortable = getSortable || function(){return this;};
-        
+
         var placements = this.map(function(){
-            
+
             var sortElement = getSortable.call(this),
                 parentNode = sortElement.parentNode,
-                
+
                 // Since the element itself will change position, we have
                 // to have some way of storing it's original position in
                 // the DOM. The easiest way is to have a 'flag' node:
@@ -62,76 +62,80 @@ jQuery.fn.sortElements = (function(){
                     document.createTextNode(''),
                     sortElement.nextSibling
                 );
-            
+
             return function() {
-                
+
                 if (parentNode === this) {
                     throw new Error(
                         "You can't sort elements if any one is a descendant of another."
                     );
                 }
-                
+
                 // Insert before flag:
                 parentNode.insertBefore(this, nextSibling);
                 // Remove flag:
                 parentNode.removeChild(nextSibling);
-                
+
             };
-            
+
         });
-       
+
         return sort.call(this, comparator).each(function(i){
             placements[i].call(getSortable.call(this));
         });
-        
+
     };
-    
+
 })();
 
 
-$(".pagination-block").first().after('Hide Recent <input type="checkbox" id="hideRecent"> | Hide Offline <input type="checkbox" id="hideOffline">');
+$(".pagination-block").first().after('Hide non VEF <input type="checkbox" id="non-vef"> | Hide Recent <input type="checkbox" id="hideRecent"> | Hide Offline <input type="checkbox" id="hideOffline">');
 
 $("#hideRecent").click(function(e) {
-  $(".online-status-recent").closest("tr").toggle();  
+    $(".online-status-recent").closest("tr").toggle();
 });
 
 $("#hideOffline").click(function(e) {
-  $(".online-status-offline").closest("tr").toggle();  
+    $(".online-status-offline").closest("tr").toggle();
+});
+
+$("#non-vef").click(function(e) {
+    $(".column-price").not(":contains('VEF')").closest("tr").toggle();
 });
 
 var table = $('.table-bitcoins').first();
-    
-    $('.header-price').first()
-        .wrapInner('<span title="sort this column"/>')
-        .each(function(){
-            
-            var th = $(this),
-                thIndex = th.index(),
-                inverse = false;
-          
-            th.click(function(){
-                
-                table.find('td').filter(function(){
-                    
-                    return $(this).index() === thIndex;
-                    
-                }).sortElements(function(a, b){
-                    return Number(jQuery.trim($.text([a])).split(" ")[0]) > Number(jQuery.trim($.text([b])).split(" ")[0]) ?
-                        inverse ? -1 : 1
-                        : inverse ? 1 : -1;
-                    
-                }, function(){
-                    
-                    // parentNode is the element we want to move
-                    return this.parentNode; 
-                    
-                });
-                
-                inverse = !inverse;
-                    
-            });
-                
+
+$('.header-price').first()
+    .wrapInner('<span title="sort this column"/>')
+    .each(function(){
+
+    var th = $(this),
+        thIndex = th.index(),
+        inverse = false;
+
+    th.click(function(){
+
+        table.find('td').filter(function(){
+
+            return $(this).index() === thIndex;
+
+        }).sortElements(function(a, b){
+            return Number(jQuery.trim($.text([a]).replace(/,/g, '') ).split(" ")[0]) > Number(jQuery.trim($.text([b]).replace(/,/g, '') ).split(" ")[0]) ?
+                inverse ? -1 : 1
+            : inverse ? 1 : -1;
+
+        }, function(){
+
+            // parentNode is the element we want to move
+            return this.parentNode;
+
         });
+
+        inverse = !inverse;
+
+    });
+
+});
 
 $(".header-price").first().hover(function() {
     $(this).css('cursor','pointer');
@@ -141,4 +145,5 @@ $(".header-price").first().hover(function() {
 $('.header-price').click();
 if (window.location.href.indexOf("sell") > -1) {
     $('.header-price').click();
+    $('#non-vef').click();
 }
